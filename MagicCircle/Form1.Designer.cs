d93e68fafcf9b2774f8610b5189ad3103e5ec0b1
NewDelion <el.emena.crea@gmail.com>
2016-08-08 01:00:12 +0900

    InitialCommit

---
+++ MagicCircle/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace MagicCircle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(358, 358);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 382);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ MagicCircle/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MagicCircle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            magic.center_x = 200;
+            magic.center_y = 200;
+            magic.radius = 90;
+            magic.distance_from_center = 0;
+            magic.rotate = false;
+            magic.has_frame = true;
+
+            MC child1 = new MC()
+            {
+                radius = 24,
+                distance_from_center = 0,
+                rotate = false,
+                has_frame = false
+            };
+
+            MC child2 = new MC()
+            {
+                radius = 33,
+                distance_from_center = 57,
+                rotate = true,
+                rotation_amount = -1,
+                has_frame = true
+            };
+
+            MC child3 = new MC()
+            {
+                radius = 10,
+                distance_from_center = 17,
+                rotate = true,
+                rotation_amount = 5,
+                has_frame = true
+            };
+
+            MC child4 = new MC()
+            {
+                radius = 35,
+                distance_from_center = 90,
+                rotate = false,
+                rotation = -60,
+                has_frame = true
+            };
+
+            MC_Orb child5 = new MC_Orb()
+            {
+                radius = 6,
+                distance_from_center = 17,
+                rotation_amount = 5
+            };
+
+            MC_Orb child6 = new MC_Orb()
+            {
+                radius = 5,
+                distance_from_center = 57,
+                rotation_amount = -1
+            };
+
+            magic.AddChild(child1);
+            magic.AddChild(child6);
+            child6.AddChild(child5);
+            magic.AddChild(child4);
+        }
+
+        MC magic = new MC();
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+            magic.Tick();
+            magic.Draw(g);
+        }
+    }
+
+    public class MC
+    {
+        public MC owner { get; set; }
+        public float center_x { get; set; }
+        public float center_y { get; set; }
+        public float radius { get; set; }
+        public float distance_from_center { get; set; }
+        public bool rotate { get; set; }
+        public float rotation { get; set; }
+        public float rotation_amount { get; set; }
+        public bool has_frame { get; set; }
+        public List<MC> child { get; set; }
+
+        public void AddChild(MC child)
+        {
+            if (this.child == null)
+                this.child = new List<MC>();
+
+            PointF center = GetPosition();
+            child.center_x = center.X;
+            child.center_y = center.Y;
+            child.owner = this;
+            this.child.Add(child);
+        }
+
+        public PointF GetPosition()
+        {
+            double radian = this.rotation * Math.PI / 180.0;
+            float result_x = this.center_x + this.distance_from_center * (float)Math.Cos(radian);
+            float result_y = this.center_y + this.distance_from_center * (float)Math.Sin(radian);
+
+            return new PointF(result_x, result_y);
+        }
+
+        public void UpdateCenter()
+        {
+            var owner_center = this.owner.GetPosition();
+            this.center_x = owner_center.X;
+            this.center_y = owner_center.Y;
+            if (this.child != null)
+                this.child.ForEach(d => d.UpdateCenter());
+        }
+
+        public void Rotate()
+        {
+            this.rotation += this.rotation_amount;
+            if (this.rotation > 360)
+                this.rotation -= 360;
+            if (this.rotation < 0)
+                this.rotation += 360;
+            if (this.child != null)
+                this.child.ForEach(d => d.UpdateCenter());
+        }
+
+        public virtual void Tick()
+        {
+            if (this.rotate)
+                Rotate();
+            if (this.child != null)
+                this.child.ForEach(d => d.Tick());
+        }
+
+        public virtual void Draw(Graphics g)
+        {
+            PointF pos = GetPosition();
+            if (this.has_frame)
+                g.DrawEllipse(Pens.Red, pos.X - this.radius, pos.Y - this.radius, this.radius * 2, this.radius * 2);
+            if (this.child != null)
+                this.child.ForEach(d => d.Draw(g));
+            
+        }
+    }
+
+    public class MC_Orb : MC
+    {
+        public override void Tick()
+        {
+            Rotate();
+            if (this.child != null)
+                this.child.ForEach(d => d.Tick());
+        }
+        public override void Draw(Graphics g)
+        {
+            PointF pos = GetPosition();
+            g.FillEllipse(Brushes.Green, pos.X - this.radius, pos.Y - this.radius, this.radius * 2, this.radius * 2);
+            if (this.child != null)
+                this.child.ForEach(d => d.Draw(g));
+        }
+    }
+}
